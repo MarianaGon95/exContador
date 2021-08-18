fc27a7f0263717702311cf57f6d90c7fc092d18c
Mariana Gonçalves <68264920+MarianaGon95@users.noreply.github.com>
2021-08-18 18:47:33 -0300

    Contador
    Exercício feito na live contador simples em JavaScript

---
+++ contador.js
@@ -0,0 +1,46 @@
+window.addEventListener("load",() => {
+
+    const botao = document.getElementById("botao");
+    botao.addEventListener("click",() => {
+
+    
+  
+
+// Tempo em segundos 
+let sec = 360;
+
+const countDiv = document.getElementById("timer");
+
+const secpass = () => {
+
+    let min = Math.floor(sec/60);
+    let segundosRestantes = sec % 60;
+
+    if(segundosRestantes < 10){
+        segundosRestantes = "0" + segundosRestantes;
+    }
+
+    if (min < 10){
+        min = "0" +  min;
+    }
+    //  Vai gerar o formato de 02:55
+
+    countDiv.innerHTML = min + ":" + segundosRestantes;
+
+    //condição final
+
+    if(sec > 0){
+
+      sec = sec -1;
+
+    } else{
+
+    countDiv.innerHTML = "Acabou !!!!";
+
+    }
+
+
+};
+    const countDown = setInterval(() => secpass(),1000);
+});
+});
